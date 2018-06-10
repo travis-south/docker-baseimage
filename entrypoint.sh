b33a951925ccae4ba93fc243c72b4dde70af6aaa
Irvin Capagcuan <irvin@capagcuan.org>
2018-06-10 21:43:34 +0800

    Added non-unique flag.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-groupmod -g ${PGID} daker
-usermod -u ${PUID} daker
-usermod -p "*" daker
+groupmod -g ${PGID} -o daker
+usermod -u ${PUID} -o daker
 mkdir -p /home/daker/.docker-workspace
 chown -R daker:daker /home/daker/.docker-workspace
 
